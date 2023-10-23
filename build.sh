96690844d7b703a1ee69a468e627371edfa2f1e1
Yiran Wei <49865495+vctorwei@users.noreply.github.com>
2023-10-23 16:16:37 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Create a "tag" or name for the image
+docker_tag=aicregistry:5000/${USER}:image-unet
+
+# build the image calling on your Dockerfile
+docker build . -f Dockerfile \
+ --network=host \
+ --tag ${docker_tag} \
+ --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg USER=${USER}
+docker push ${docker_tag}
